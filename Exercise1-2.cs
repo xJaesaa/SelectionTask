9052afc59ad7bbb2d88c409fd2e53fde7e3f0776
xJaesaa <103669129@student.swin.edu.au>
2021-03-18 19:18:49 +1100

    exercises 1.2, 1.3 and 2

---
+++ Exercise1-2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace JaesaCodePog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Age");
+            string ageStr = Console.ReadLine();
+
+            int age = int.Parse(ageStr);
+
+            Console.WriteLine("Enter Your Gender (M, F, NB, Other)");
+            string gender = Console.ReadLine();
+
+            if(age >= 18){
+                Console.WriteLine("u " + gender + " are an adult");
+            }
+            else{
+                Console.WriteLine("too young, begone " + gender + " child");
+            }
+            
+        }
+    }
+}
---
+++ Exercise2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace JaesaCodePog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("For testing purposes Username and Password are: 'bruh'");
+            Console.WriteLine("Enter Username");
+            string username = Console.ReadLine();
+
+            if(username == "bruh")
+            {
+                Console.WriteLine("Enter Password");
+                string password = Console.ReadLine();
+
+                if(password == "bruh")
+                {
+                    Console.WriteLine("Logged in baby");
+                }
+                else
+                {
+                    Console.WriteLine("wrong password bruz");
+                }
+            }
+            else{
+                Console.WriteLine("Username does not exist");
+            }
+            
+        }
+    }
+}
